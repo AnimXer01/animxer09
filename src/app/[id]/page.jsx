2effa83b2323b7393937f6b2a7dc5d588a822758
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-02-03 08:58:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/[id]/page.jsx
@@ -0,0 +1,34 @@
+import { getTopAnimes, makeRequest } from "@/API/request";
+import Link from "next/link";
+import AnimeInfo from "./../Components/AnimeInfo";
+import Episodes from "./../Components/Episodes";
+import PopularAnimes from "./../Components/PopularAnimes";
+import ErrorPage from "./../Components/ErrorPage";
+
+const page = async ({ params }) => {
+  const anime = await makeRequest(`/info/${params?.id}`, { revalidate: 60 });
+  const popular = await getTopAnimes();
+  return anime?.id !== undefined ? (
+    <div className="color-text my-5 mx-0 grid max-w-screen-2xl grid-cols-1 gap-2 md:mx-auto md:gap-5 md:px-16 md:py-16 lg:grid-cols-[70%,30%]">
+      <div>
+        {/* Navigation Link */}
+        <div className="background mb-2 flex h-max w-full gap-2 rounded-none md:rounded bg-[#fff1] px-5 py-3 text-sm">
+          <Link href="/" className="hover:text-purple-500">
+            Home
+          </Link>
+          <span>/</span>
+          <p className="line-clamp-1">{anime?.title}</p>
+        </div>
+        {/* Manga Info */}
+        <AnimeInfo anime={anime} />
+        {/* Manga Chapters */}
+        <Episodes episodes={anime?.episodes} animeId={anime?.id} />
+      </div>
+      <PopularAnimes animes={popular} />
+    </div>
+  ) : (
+    <ErrorPage />
+  );
+};
+
+export default page;
