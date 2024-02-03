b25b0214f3576791c1a60d95ee3c643ffa89711d
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-02-03 18:27:19 +0800

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,7 +8,7 @@ import Footer from "./Components/Footer";
 const roboto = Roboto_Condensed({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "AnimeSensei",
+  title: "ANIMXER",
   description: "All Your Favorite Anime In One Site.",
 };
 
